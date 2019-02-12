9b2b8ec894d01d6fb26f16b0023855e858fbfa96
acc15 <vyacheslav.mayorov@gmail.com>
2019-02-12 04:50:30 +0300

    content-disposition tests

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -5,9 +5,15 @@ import {getFileNameFromCD} from "./utils";
 describe("utils", () => {
     describe("getFileNameFromCD", () => {
         it("must correctly parse Content-Disposition header", () => {
-            expect(getFileNameFromCD("attachment; filename=[limetorrents.info]Will.and.Grace.S10E10.720p.WEB.x264-300MB.torrent"))
-                .eq("[limetorrents.info]Will.and.Grace.S10E10.720p.WEB.x264-300MB.torrent");
+            expect(getFileNameFromCD("attachment; filename=\"[sq]withquotes.torrent\""))
+                .eq("[sq]withquotes.torrent");
+
+            expect(getFileNameFromCD("attachment; filename*=UTF-8''weird%20%23%20%80%20%3D%20%7B%20%7D%20%3B%20filename.txt"))
+                .eq("weird # � = { } ; filename.txt");
+
+            expect(getFileNameFromCD("attachment; filename=[sb]without-quotes.torrent"))
+                .eq("[sb]without-quotes.torrent");
+
         });
     });
-
 });
